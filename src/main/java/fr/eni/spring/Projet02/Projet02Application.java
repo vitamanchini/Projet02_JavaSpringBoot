06983af2a2eedc3619305f787cffe98f9f3ffb19
vitamanchini <vitamanchini@gmail.com>
2024-03-25 11:42:08 +0100

    first

---
+++ src/main/java/fr/eni/spring/Projet02/Projet02Application.java
@@ -0,0 +1,13 @@
+package fr.eni.spring.Projet02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projet02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projet02Application.class, args);
+	}
+
+}
---
+++ src/test/java/fr/eni/spring/Projet02/Projet02ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.spring.Projet02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projet02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
